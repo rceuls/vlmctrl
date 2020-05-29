5692e36f7d5c48bfb8759a7593b418bbd6f69e43
Raf Ceuls <rceuls@gmail.com>
2020-05-29 22:17:44 +0200

    initial draft

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/rceuls/vlmctrl
+
+go 1.13
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/micro/go-micro/v2 v2.7.0
+	rsc.io/quote v1.5.2
+)
---
+++ vol_service.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+const mqttTopic = "vct_volume/commands"
+const mqttBroker = "tcp://localhost:1883"
+const mqttClientID = "vctr_service"
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func cleanup(client mqtt.Client) {
+
+	if token := client.Unsubscribe(mqttTopic); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	client.Disconnect(250)
+	log.Println("Disconnected client")
+}
+
+func setupCloseHandler(client mqtt.Client) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanup(client)
+		os.Exit(0)
+	}()
+}
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+
+	// mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	mqtt.ERROR = log.New(os.Stdout, "", 0)
+
+	opts := mqtt.NewClientOptions().AddBroker(mqttBroker).SetClientID(mqttClientID)
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	c := mqtt.NewClient(opts)
+	setupCloseHandler(c)
+
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := c.Subscribe(mqttTopic, 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	log.Print("Starting polling as pid ", os.Getpid())
+
+	for {
+		time.Sleep(10 * time.Millisecond)
+	}
+}
